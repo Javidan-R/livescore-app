11c3f8b910d344028f5dfcb772803f811524afa1
Javidan-R <rajablijavidan@gmail.com>
2025-06-21 20:36:29 +0400

    Initial Commit

---
+++ LivescoreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LivescoreApp
+//
+//  Created by Javidan Rajabli on AH 24.12.1446.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
